fcfac7d1669ae6815c1c849a9a4ebd17e7607728
TINHOLINDO <134516651+TINHOLINDO@users.noreply.github.com>
2024-08-14 10:39:11 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,18 @@
+// Variáveis
+// tipo Number
+
+const meuNumero = 6;
+const numeroUm = 7;
+const numeroDois = 5;
+
+const soma = 3+6
+console.log (soma)
+
+
+const subtracao = numeroQuatro - meuNumero
+console.log(subtracao)
+
+const multiplicacao = 6 * 7;
+console.log(multiplicacao);
+
+const divisao = meuNumero / numeroNove; 

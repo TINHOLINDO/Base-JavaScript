ff194430e8ff9c1f5f7bd0815d86a5edacf2bdf4
TINHOLINDO <134516651+TINHOLINDO@users.noreply.github.com>
2024-08-20 10:58:42 -0300

    Update variaveis.js

--- variaveis.js
+++ variaveis.js
@@ -3,16 +3,59 @@
 
 const meuNumero = 6;
 const numeroUm = 7;
-const numeroDois = 5;
-
-const soma = 3+6
-console.log (soma)
+const numeroDois = 2;
 
 
-const subtracao = numeroQuatro - meuNumero
+const soma = numeroUm + meuNumero;
+console.log(soma)
+
+const subtracao = numeroUm - meuNumero;
 console.log(subtracao)
 
-const multiplicacao = 6 * 7;
-console.log(multiplicacao);
+const multiplicacao = numeroDois * 10;
+console.log(multiplicacao)
 
-const divisao = meuNumero / numeroNove; 
+const divisao = meuNumero / numeroDois
+console.log(divisao)
+
+//Float ( Número decimal )
+
+const numeroTres = 3.3;
+const numeroQuatro = .5  // mesma coisa que 0.5
+
+const operacaoM = numeroTres * numeroQuatro
+console.log(operacaoM)
+
+// NaN not a Number Não é um número
+
+const operacaoN = 'a' * meuNumero
+console.log(operacaoN)
+
+// concatenação
+const novaOperacao = "a" + meuNumero
+console.log(novaOperacao)
+
+// string (texto)
+
+const meuNome = 'Andre'
+const frase = 'Meu nome é: '
+const issoNaoEumNumero = '50'
+
+const minhaFrase = frase + meuNome
+console.log(minhaFrase);
+
+const operacaoO = issoNaoEumNumero + meuNumero;
+console.log(operacaoO)
+
+// Boolean
+// True -> verdadeiro;
+// False -> falso;
+
+const comparacao = '6' === meuNumero
+console.log(comparacao)
+
+const comparacaoII = '6' === meuNumero
+console.log(comparacaoII)
+
+const comparacaoIII = '6' == meuNumero
+console.log(comparacaoIII);
